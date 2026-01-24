0cf98c910f66684a83866e7afef5e62bd62675cf
Alex Kavanagh <alexkavanagh6@gmail.com>
2026-01-24 18:20:57 +0000

    Update main

--- main.rb
+++ main.rb
@@ -1,11 +1,25 @@
 #require "lib/"
-require 'json'
+#require 'json'
 
 #File.open("google-10000-english-no-swears.txt", 'r') do |file|
 
   #puts file.read()
 #end
 lines = File.readlines("google-10000-english-no-swears.txt")
-json = JSON.generate(lines)
-puts json
+#json = JSON.generate(lines)
+words = []
+for line in lines do
+  words.push(line)
+end
+#puts json
+
+random_word = words.sample()
+
+puts random_word
+
+guess_length = random_word.length-1
+
+guess_length.times do
+puts "_"
+end
 #puts "Welcome to Hangman"
